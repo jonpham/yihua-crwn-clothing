9b7d8d15ee0b0539412617be1bb46b382717fa4c
Jonathan Pham <jonathan@paysa.com>
2020-05-25 14:08:02 -0700

    Decouple CategoryDirectory from homepage, store section data as state in CategoryDirectory.

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
-import MenuItem from '../../components/MenuItem';
+import CategoryDirectory from '../../components/CategoryDirectory';
 
 import './Homepage.scss';
 
 const Homepage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <MenuItem title="HATS" />
-        <MenuItem title="JACKETS" />
-        <MenuItem title="SNEAKERS" />
-        <MenuItem title="MENS" />
-        <MenuItem title="WOMENS" />
-      </div>
+      <CategoryDirectory />
     </div>
   );
 };
