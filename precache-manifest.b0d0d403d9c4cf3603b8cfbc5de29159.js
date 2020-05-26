6d1408252566f42783fcaabfbb2bdb4609afacad
Jonathan Pham <jonathan@paysa.com>
2020-05-25 18:26:26 -0700

    Updates

---
+++ precache-manifest.b0d0d403d9c4cf3603b8cfbc5de29159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a992cd8577cba8070cefe456c5779624",
+    "url": "/yihua-crwn-clothing/index.html"
+  },
+  {
+    "revision": "dbcd27355a6a7b13277d",
+    "url": "/yihua-crwn-clothing/static/css/main.86a03fce.chunk.css"
+  },
+  {
+    "revision": "a2f9229e9bb7e7414ff2",
+    "url": "/yihua-crwn-clothing/static/js/2.98dfe06c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yihua-crwn-clothing/static/js/2.98dfe06c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbcd27355a6a7b13277d",
+    "url": "/yihua-crwn-clothing/static/js/main.05e03d9f.chunk.js"
+  },
+  {
+    "revision": "8e34de08d900b88a6003",
+    "url": "/yihua-crwn-clothing/static/js/runtime-main.e9046992.js"
+  }
+]);
