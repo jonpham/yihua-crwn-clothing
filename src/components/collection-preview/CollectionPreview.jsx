ee56753155b6548dd98b0fab80229b4cf3bf898b
Jonathan Pham <jonathan@paysa.com>
2020-05-26 12:55:22 -0700

    Limit collection preview to 4 items.

--- src/components/collection-preview/CollectionPreview.jsx
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -1,17 +1,22 @@
 import React, { memo } from 'react';
 
+const MAX_ITEM_PREVIEW = 4;
+
 const CollectionPreview = ({ items, title }) => {
   return (
     <div className="CollectionPreview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
-        {items.map((item) => {
-          return (
-            <div key={item.id}>
-              {item.name}
-            </div>
-          );
-        })}
+        {items
+          .filter((item, idx) => idx < MAX_ITEM_PREVIEW)
+          .map((item) => {
+            return (
+              <div key={item.id}>
+                {item.name}
+              </div>
+            );
+          })
+        }
       </div>
     </div>
   );
