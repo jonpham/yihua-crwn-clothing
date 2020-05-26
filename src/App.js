dee1df02de078dbb0be53439bc5d1ea30e38a86e
Jonathan Pham <jonathan@paysa.com>
2020-05-26 15:49:33 -0700

    Add Header with SVG logo.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/home-page';
 import ShopPage from './pages/shop-page';
+import Header from './components/header';
+
 import './App.scss';
 
 const App = () => {
   return (
     <div className="crwn-clothing">
+      <Header />
       <Switch>
         <Route exact={true} path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import React, { memo } from 'react';
+
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/crwn-logo.svg';
+
+import './Header.scss';
+
+const Header = () => (
+  <div className='header'>
+    <Link className="logo-container" to="/">
+      <Logo />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>Shop</Link>
+      <Link className='option' to='/contact'>Contact</Link>
+    </div>
+  </div>
+);
+
+export default memo(Header);
